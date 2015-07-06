a2a71a9af2218249c0a73c30a0eebbc5251b1d65
root <epanchee@gmail.com>
2015-07-06 19:22:47 +0500

    minor fixes

--- src/WordCount.java
+++ src/WordCount.java
@@ -22,10 +22,10 @@ public class WordCount extends Configured implements Tool {
     public int run( String[] args) throws  Exception {
         Job job  = Job .getInstance(getConf(), " wordcount ");
         job.setJarByClass( this .getClass());
-        FileInputFormat.addInputPaths(job, String.valueOf(new Path(args[ 0])));
+        FileInputFormat.addInputPaths(job, String.valueOf(new Path(args[0])));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         job.setMapperClass(Map.class);
-        job.setReducerClass(Map.Reduce.class);
+        job.setReducerClass(Reduce.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
@@ -33,32 +33,33 @@ public class WordCount extends Configured implements Tool {
     }
 
     public static class Map extends Mapper<LongWritable ,  Text ,  Text ,  IntWritable > {
-        private final static IntWritable one  = new IntWritable( 1);
-        private Text word  = new Text();
-        private static final Pattern WORD_BOUNDARY = Pattern .compile(" \\ s* \\ b \\ s* ");
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+        private static final Pattern WORD_BOUNDARY = Pattern.compile(" \\ s* \\ b \\ s* ");
 
-        public void map( LongWritable offset,  Text lineText,  Context context)
-                throws  IOException,  InterruptedException {
-            String line  = lineText.toString();
-            Text currentWord  = new Text();
-            for ( String word  : WORD_BOUNDARY .split(line)) {
+        public void map(LongWritable offset, Text lineText, Context context)
+                throws IOException, InterruptedException {
+            String line = lineText.toString();
+            Text currentWord = new Text();
+            for (String word : WORD_BOUNDARY.split(line)) {
                 if (word.isEmpty()) {
                     continue;
                 }
-                currentWord  = new Text(word);
-                context.write(currentWord,one);
-            }
-        }
-
-        public static class Reduce extends Reducer<Text ,  IntWritable ,  Text ,  IntWritable > {
-            @Override public void reduce( Text word,  Iterable<IntWritable > counts,  Context context)
-                    throws IOException,  InterruptedException {
-                int sum  = 0;
-                for ( IntWritable count  : counts) {
-                    sum  += count.get();
-                }
-                context.write(word,  new IntWritable(sum));
+                currentWord = new Text(word);
+                context.write(currentWord, one);
             }
         }
     }
+
+        public static class Reduce extends Reducer<Text ,  IntWritable ,  Text ,  IntWritable > {
+            @Override
+            public void reduce(Text word, Iterable<IntWritable> counts, Context context)
+                    throws IOException, InterruptedException {
+                int sum = 0;
+                for (IntWritable count : counts) {
+                    sum += count.get();
+                }
+                context.write(word, new IntWritable(sum));
+            }
+        }
 }
